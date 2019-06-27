07d8a1299bfcae315e0c1156cbf053b67c3fba81
Kyle Graehl <kyle@chartio.com>
2019-06-26 20:04:58 -0700

    bundle dependencies and import using browserify

---
+++ makedeps/index.js
@@ -0,0 +1,33 @@
+var m
+
+m = require('react')
+window.React = m
+
+m = require('react-dom')
+window.ReactDOM = m
+
+m = require('prop-types')
+window.PropTypes = m
+
+m = require('redux')
+window.Redux = m
+
+m = require('react-redux')
+window.ReactRedux = m
+
+m = require('redux-thunk')
+window.ReduxThunk = m
+
+m = require('redux-logger')
+window.reduxLogger = m
+
+m = require('@material-ui/core')
+window.MaterialUI = m
+
+m = require('bro-fs')
+window.fs = m
+
+m = require('underscore')
+window._ = m
+
+module.exports = {}
